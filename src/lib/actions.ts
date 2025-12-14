c89061dbda2b9d00b57dfbc36440b2183f6e2854
antameng <671608311@qq.com>
2025-12-14 21:33:18 +0800

    feat: implement story creation functionality with image upload and user authentication

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,7 +1,7 @@
 "use server"
 import { auth } from "@clerk/nextjs/server"
 import prisma from "./client"
-import z, { success } from "zod"
+import { z } from "zod"
 import { revalidatePath } from "next/cache"
 
 export const switchFollow = async (userId: string) => {
@@ -280,3 +280,39 @@ export const addPost = async (formData: FormData, img: string) => {
     throw new Error('Something went wrong!')
   }
 }
+
+export const addStory = async (img: string) => {
+  const { userId } = await auth();
+
+  if (!userId) throw new Error("User is not authenticated!");
+
+  try {
+    const existingStory = await prisma.story.findFirst({
+      where: {
+        userId,
+      },
+    });
+
+    if (existingStory) {
+      await prisma.story.delete({
+        where: {
+          id: existingStory.id,
+        },
+      });
+    }
+    const createdStory = await prisma.story.create({
+      data: {
+        userId,
+        img,
+        expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    return createdStory;
+  } catch (err) {
+    console.log(err);
+  }
+};
